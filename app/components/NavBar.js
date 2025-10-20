47cf557cfc72fc0d7a05ee5e43ff570be9343c03
AdrianKnudsen <adrian@knudsen.no>
2025-10-20 11:25:29 +0200

    css changes
    small css changes

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -30,7 +30,7 @@ export default function NavBar({
         style={{
           height: expanded ? "20rem" : "6.2em",
           transition: "height 1s cubic-bezier(0.4, 0, 0.2, 1)",
-          overflow: "auto",
+          overflow: "hidden",
         }}
       >
         <Image
